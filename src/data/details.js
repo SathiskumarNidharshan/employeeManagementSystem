7785076f70ac3b1fca8e4bfcf22c969dc38f21df
Sathiskumar Nidharshan <nitharshan23s@gmail.com>
2023-02-09 19:57:08 +0530

    first

---
+++ src/data/details.js
@@ -0,0 +1,46 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Samith',
+      lastName: 'Bandara',
+      email: 'samith@example.com',
+      salary: '75000',
+      date: '2022-01-11'
+    },
+    {
+      id: 2,
+      firstName: 'Suresh',
+      lastName: 'Mali',
+      email: 'suresh@example.com',
+      salary: '80000',
+      date: '2022-01-15'
+    },
+    {
+      id: 3,
+      firstName: 'Sathis',
+      lastName: 'Nidha',
+      email: 'sathish@example.com',
+      salary: '79000',
+      date: '2022-01-17'
+    },
+    {
+      id: 4,
+      firstName: 'Kenady',
+      lastName: 'Jeyasooriya',
+      email: 'kenady@example.com',
+      salary: '56000',
+      date: '2022-01-25'
+    },
+    {
+      id: 5,
+      firstName: 'Arun',
+      lastName: 'Vikky',
+      email: 'arun@example.com',
+      salary: '65000',
+      date: '2022-01-30'
+    },
+    
+  ];
+  
+  export { employeesData };
+  
